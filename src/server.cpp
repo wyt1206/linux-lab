5e899293c7570e7f35757c2b8c70e622b3fb09e9
rdw <rdw@rdwdeMacBook-Air.local>
2026-07-09 14:11:55 +0800

    Step 2: Create TCP socket

--- src/server.cpp
+++ src/server.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <sys/socket.h>
+#include <unistd.h>
 
 int main() {
-    std::cout << "Hello Linux!" << std::endl;
+
+    // Create a TCP socket
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (server_fd == -1) {
+        std::cerr << "Failed to create socket" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Socket created successfully!" << std::endl;
+    std::cout << "Socket FD: " << server_fd << std::endl;
+
+
+    // Close socket
+    close(server_fd);
+
     return 0;
 }
